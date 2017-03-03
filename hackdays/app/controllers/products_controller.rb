b9a80e20ea21223dc434805ccb0bc9b1d7b53496
ekerby <emilykerby@gmail.com>
2017-03-03 13:16:35 -0800

    adds customer login

--- hackdays/app/controllers/products_controller.rb
+++ hackdays/app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
 
+#makes them login before they can do stuff
+    # before_filter :authorize
+
   def index
     @products = Product.all.order(created_at: :desc)
   end
--- hackdays/db/schema.rb
+++ hackdays/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302073028) do
+ActiveRecord::Schema.define(version: 20170303200059) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170302073028) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
